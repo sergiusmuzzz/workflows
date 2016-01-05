6f8a7703405ab8cee63f5dbc0e7dfac13df66c80
Sergey <sishutin@nexon.net>
2016-01-05 14:36:34 -0800

    Crushed JSON file

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
 	gulpif = require('gulp-if'),
 	uglify = require('gulp-uglify'),
 	htmlmin = require('gulp-htmlmin'),
+	jsonminify = require('gulp-jsonminify'),
 	concat = require('gulp-concat');
 
 var env,
@@ -75,7 +76,7 @@ gulp.task('watch', function(){
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
 	gulp.watch('builds/development/*.html', ['html']);
-	gulp.watch(jsonSources, ['json']);
+	gulp.watch('builds/development/js/*.json', ['json']);
 });
 
 gulp.task('html', function(){
@@ -86,7 +87,9 @@ gulp.task('html', function(){
 });
 
 gulp.task('json', function(){
-	gulp.src(jsonSources)
+	gulp.src('builds/development/js/*.json')
+	.pipe(gulpif(env === 'production', jsonminify()))
+	.pipe(gulpif(env === 'production', gulp.dest('builds/production/js')))
 	.pipe(connect.reload())
 });
 
